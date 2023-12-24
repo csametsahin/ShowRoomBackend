79adc2c497b100fb3dcf6261aa5cf018089c3320
c9013283 <c9013283@arcelik.com>
2023-12-24 23:00:11 +0300

    Add project files.

---
+++ SR.Entities/Concrete/BaseEntities/BaseEntity.cs
@@ -0,0 +1,18 @@
+﻿using SR.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SR.Entities.Concrete.BaseEntities
+{
+    public class BaseEntity : IEntity
+    {
+        public int Id { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+        public DateTime? UpdatedDate { get; set; }
+        public string? CreatedBy { get; set; }
+        public string? UpdatedBy { get; set; }
+    }
+}
---
+++ SR.Entities/Concrete/DbModels/User.cs
@@ -0,0 +1,20 @@
+﻿using SR.Entities.Concrete.BaseEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SR.Entities.Concrete.DbModels
+{
+    public class User : BaseEntity
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string MailAddress { get; set; }
+        public string Password { get; set; }
+        public int PlanId { get; set; }
+        public bool IsPaymentGranted { get; set; }
+        public bool IsMailApproved { get; set; }
+    }
+}
