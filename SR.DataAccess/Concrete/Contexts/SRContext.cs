83c7bbff713930ade7bd0ee6dc28c30d9057fe3e
csametsahin <mcnblogger@gmail.com>
2023-12-29 19:48:42 +0300

    db context added

---
+++ SR.DataAccess/Concrete/Contexts/SRContext.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Builder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace SR.DataAccess.Concrete.Contexts
+{
+    public class SRContext : DbContext
+    {
+        public SRContext()
+        {
+        }
+
+        public SRContext(DbContextOptions<DbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                var builder = WebApplication.CreateBuilder();
+                var connectionString = builder.Configuration.GetSection("ConnectionStrings:SRConnectionString").Value;
+                optionsBuilder
+                    .UseLazyLoadingProxies()
+                    .UseSqlServer(connectionString, conn => conn.CommandTimeout(180))
+                    .EnableSensitiveDataLogging()
+                    .LogTo(Console.WriteLine, LogLevel.Information)
+                    .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+            }
+        }
+
+        #region set models
+        // public DbSet<User> Users {get; set;}
+        #endregion
+    }
+}
